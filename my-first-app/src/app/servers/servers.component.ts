a3e1b4f2c52252e8f1c5eec9db9db6f58b715974
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2020-05-13 16:20:19 +1000

    using two way databinding - ngModel

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server created';
+  serverName: string = 'enter server name';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
   onServerCreated() {
-    this.serverCreationStatus = 'New Server created successfully!';
+    this.serverCreationStatus =
+      'New Server created successfully! Name is ' + this.serverName;
+    this.serverName = 'enter server name';
+  }
+  onUpdateServerName(event: any) {
+    //console.log(event);
+    this.serverName = event.target.value;
   }
   ngOnInit(): void {}
 }
