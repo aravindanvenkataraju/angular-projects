a6210b6f4e3b442b02bedf1c492047b567b11c7e
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-08 19:17:09 +1100

    component binding assignment added

--- cmp-databinding/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,20 +1,31 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent implements OnInit {
+export class CockpitComponent implements OnInit, OnChanges, OnDestroy {
   @Output() serverCreated = new EventEmitter<{name: string, content: string}>();
   @Output() blueprintCreated = new EventEmitter<{name: string, content: string}>();
   // newServerName = '';
   // newServerContent = '';
   @ViewChild('newServerContent', {static: true}) newServerContentElement: ElementRef;
-  constructor() { }
+  constructor() { 
+    console.log('Constructor called');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
   }
+  
 
   onAddServer(newServerNameElement: HTMLInputElement){
     this.serverCreated.emit({name: newServerNameElement.value, content: this.newServerContentElement.nativeElement.value});
@@ -22,4 +33,6 @@ export class CockpitComponent implements OnInit {
   onAddBlueprint(newServerNameElement: HTMLInputElement){
     this.blueprintCreated.emit({name: newServerNameElement.value, content: this.newServerContentElement.nativeElement.value});
   }
+
+
 }
