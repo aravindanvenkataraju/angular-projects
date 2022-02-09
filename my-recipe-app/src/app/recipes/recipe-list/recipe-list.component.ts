672dec23a34986b2afe7c9f6963920b220280923
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-10 08:30:42 +1100

    my-recipe-app, display selected recipe on the right

--- my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() onRecipeSelected: EventEmitter<Recipe> = new EventEmitter();
   recipes: Recipe[] = [
-    new Recipe('test recipe', 'description to the recipe', 'https://picturetherecipe.com/wp-content/uploads/2018/06/Indian-Style-Chicken-Cutlets-PictureTheRecipe.jpg'),
-    new Recipe('test recipe', 'description to the recipe', 'https://picturetherecipe.com/wp-content/uploads/2018/06/Indian-Style-Chicken-Cutlets-PictureTheRecipe.jpg'),
-    new Recipe('test recipe', 'description to the recipe', 'https://picturetherecipe.com/wp-content/uploads/2018/06/Indian-Style-Chicken-Cutlets-PictureTheRecipe.jpg')
+    new Recipe('test recipe 1', 'description to the recipe - 1', 'https://picturetherecipe.com/wp-content/uploads/2018/06/Indian-Style-Chicken-Cutlets-PictureTheRecipe.jpg'),
+    new Recipe('test recipe 2', 'description to the recipe - 2', 'https://picturetherecipe.com/wp-content/uploads/2018/06/Indian-Style-Chicken-Cutlets-PictureTheRecipe.jpg'),
+    new Recipe('test recipe 3', 'description to the recipe - 3', 'https://picturetherecipe.com/wp-content/uploads/2018/06/Indian-Style-Chicken-Cutlets-PictureTheRecipe.jpg')
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  recipeSelected(selectedRecipe: Recipe){
+    this.onRecipeSelected.emit(selectedRecipe);
+  }
 }
