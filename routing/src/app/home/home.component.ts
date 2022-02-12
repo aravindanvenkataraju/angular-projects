f108064c9fdb6fe90bec283fe45a3c59610a24e0
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-12 11:42:08 +1100

    routers, use candDeactivate by introducing a custom interface to be implemented by the component to be deactivated

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router, private authService: AuthService) {}
 
-  constructor(private router: Router) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  gotoServers() {
+    this.router.navigate(["/", "servers"]);
   }
 
-  gotoServers(){
-    this.router.navigate(['/', 'servers']);
+  gotoServer(id: number) {
+    this.router.navigate(["servers", id, "edit"], {
+      queryParams: { allowEdit: true },
+      fragment: "loading",
+    });
   }
 
-  gotoServer(id:number){
-    this.router.navigate(['servers', id, 'edit'], {queryParams:{allowEdit: true}, fragment: 'loading'});
+  login() {
+    this.authService.login();
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,43 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Observable } from "rxjs";
 
-import { ServersService } from '../servers.service';
+import { ServersService } from "../servers.service";
+import { CanComponentDeactivate } from "./can-deactivate-guard.service";
 
 @Component({
-  selector: 'app-edit-server',
-  templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+  selector: "app-edit-server",
+  templateUrl: "./edit-server.component.html",
+  styleUrls: ["./edit-server.component.css"],
 })
-export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-  serverName = '';
-  serverStatus = '';
-  allowEdit:boolean = false;
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
+  server: { id: number; name: string; status: string };
+  serverName = "";
+  serverStatus = "";
+  allowEdit: boolean = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, 
-    private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (
+      this.allowEdit &&
+      (this.serverName !== this.server.name ||
+        this.serverStatus !== this.server.status) &&
+      !this.changesSaved
+    ) {
+      return confirm(`Do you want to discard change?`);
+    }
 
-  ngOnInit() {
+    return true;
+  }
+
+  ngOnInit(): void {
     // this.server = this.serversService.getServer(1);
     // this.serverName = this.server.name;
     // this.serverStatus = this.server.status;
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(parseInt(params['id']));
-        this.serverName = this.server.name;
-        this.serverStatus = this.server.status;
-      }
-    );
-    this.route.queryParams.subscribe(
-      (params: Params) =>{
-        this.allowEdit = params['allowEdit'] === 'true'? true: false;
-      }
-    );
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(parseInt(params["id"]));
+      this.serverName = this.server.name;
+      this.serverStatus = this.server.status;
+    });
+    this.route.queryParams.subscribe((params: Params) => {
+      this.allowEdit = params["allowEdit"] === "true" ? true : false;
+    });
     this.route.fragment.subscribe();
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
+    this.changesSaved = true;
+    this.router.navigate(["../"], { relativeTo: this.route });
   }
-
 }
