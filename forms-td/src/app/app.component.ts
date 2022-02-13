9b782a6f0e3444320154d015039739bc25b6140c
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-14 08:08:06 +1100

    template driven forms, basic understanding

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  defaultQuestion: string = "teacher";
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  onSubmit(userform: NgForm) {
+    console.log("form submitted", userform);
   }
 }
