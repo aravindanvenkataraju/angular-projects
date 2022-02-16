8850ecb4625e82692bc66985102e0420faa10fc5
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-16 17:35:19 +1100

    my-recipe-app, use template driven form to shopping list edit component

--- my-recipe-app/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ my-recipe-app/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,24 +1,22 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/models/ingredient.model';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('ingredientNameInput') ingredientNameElement: ElementRef;
-  @ViewChild('ingredientAmountInput') ingredientAmountElement: ElementRef;
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitForm(shoppingEditForm: NgForm) {
+    const formValue = shoppingEditForm.value;
+    this.shoppingListService.addIngredient(
+      new Ingredient(formValue.ingredientName, formValue.ingredientAmount)
+    );
   }
-
-  onAddIngredient(){
-    const ingredientName = this.ingredientNameElement.nativeElement.value;
-    const ingredientAmount = this.ingredientAmountElement.nativeElement.value;
-    this.shoppingListService.addIngredient(new Ingredient(ingredientName, ingredientAmount));
-  }
-
 }
