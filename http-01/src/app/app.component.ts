d5c0fc1b06dfbcf9f70d608d9d80dc0c6d5566ea
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-20 15:23:41 +1100

    add bootstrap spinner when fetching posts

--- http-01/src/app/app.component.ts
+++ http-01/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from "./post.model";
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -31,6 +32,7 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         "https://aravindans-first-project-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
@@ -48,6 +50,7 @@ export class AppComponent implements OnInit {
       .subscribe((posts) => {
         console.log(posts);
         this.loadedPosts = posts;
+        this.isFetching = false;
       });
   }
 
