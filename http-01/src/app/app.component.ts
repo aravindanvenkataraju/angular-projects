11c13bb869bba810c0ac93d22b962fa5d22c187f
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-20 15:15:14 +1100

    http-01, post and fetch data from firebase backend.

---
+++ http-01/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs";
+import { Post } from "./post.model";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.onFetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http
+      .post<{ name: string }>(
+        "https://aravindans-first-project-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        "https://aravindans-first-project-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key))
+              postsArray.push({ ...responseData[key], id: key });
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+        this.loadedPosts = posts;
+      });
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
