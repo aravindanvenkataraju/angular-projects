fe927ca211f7b4ca6193f52d02f09a908fa6dd05
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-02-26 20:27:23 +1100

    my-recipe-app add ayth form with validations

--- my-recipe-app/src/app/components/auth/auth.component.ts
+++ my-recipe-app/src/app/components/auth/auth.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
+  isLoginMode: boolean = true;
+  authForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.authForm = new FormGroup({
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
+  toggleMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  submitAuth() {
+    console.log(this.authForm.value);
+    console.log(this.authForm.get('email').errors);
+    console.log(this.authForm.get('password').errors);
+  }
 }
